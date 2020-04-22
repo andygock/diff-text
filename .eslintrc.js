bd30d774085a3a9ed96f452c42069e68e073649f
Andy Gock <andy@gock.net>
2020-04-22 22:58:52 +1000

    Add custom config files

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    mocha: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      arrowFunctions: true,
+    },
+  },
+  extends: ['react-app', 'airbnb', 'prettier'],
+  rules: {
+    'comma-dangle': 0,
+    'jsx-a11y/label-has-for': 0,
+    'no-alert': 0,
+    'no-console': 0, // remove this for production!
+    'no-prototype-builtins': 0,
+    'no-shadow': 0,
+    'no-underscore-dangle': 0,
+    'no-unused-vars': 0,
+    'no-useless-constructor': 0,
+    'prefer-destructuring': 0,
+    'react/destructuring-assignment': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-wrap-multilines': [
+      'error',
+      { arrow: true, return: true, declaration: true },
+    ],
+    'react/no-array-index-key': 0,
+    'react/no-did-mount-set-state': 0,
+    'react/no-unused-prop-types': 0,
+    'react/no-unused-state': 0,
+    'react/prefer-stateless-function': 0,
+    'react/prop-types': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  plugins: ['react', 'react-hooks'],
+};

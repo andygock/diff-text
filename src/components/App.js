79fd84eacfa55ce719e0c16c6154dd9d5c46cdb9
Andy Gock <andy@gock.net>
2020-04-22 23:10:50 +1000

    Use bare bones starter src

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const App = () => {
+  return (
+    <div>
+      Edit <code>components/src/App.js</code> and save to reload.
+    </div>
+  );
+};
+
+App.propTypes = {};
+
+App.defaultProps = {};
+
+export default App;

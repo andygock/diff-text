28d823ee795d36e2f9fe4d9347cd0175bccf161d
Andy Gock <andy@gock.net>
2020-04-24 02:13:07 +1000

    Fix test case

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const element = getByText(/and save to reload/i);
+  const element = getByText(/diff-text/i);
   expect(element).toBeInTheDocument();
 });

258889d9e2396df0e4b85f6d70fadebe49009f71
Andy Gock <andy@gock.net>
2020-04-24 01:58:21 +1000

    Add pane swap button

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ReactDiffViewer from 'react-diff-viewer';
+import { Button } from '@blueprintjs/core';
 import TextInput from './TextInput';
 import './App.css';
 import Footer from './Footer';
@@ -31,13 +32,35 @@ const App = () => {
 
   return (
     <div className="app">
-      <p className="heading">
-        diff-text - A web app for comparing two portions of text using{' '}
-        <a href="https://github.com/praneshr/react-diff-viewer">
-          react-diff-viewer
-        </a>
-      </p>
-      <div className="grid">
+      <div className="grid-header">
+        <p className="heading">
+          diff-text - A web app for comparing two portions of text using{' '}
+          <a href="https://github.com/praneshr/react-diff-viewer">
+            react-diff-viewer
+          </a>
+          . Supports text and{' '}
+          <a href="https://github.com/sheetjs/sheetjs#file-formats">
+            common spreadsheet files
+          </a>
+          .
+        </p>
+        <div className="center">
+          <Button
+            minimal
+            onClick={() => {
+              // swap left and right contents
+              const [left, right] = [inputLeft, inputRight];
+              setInputLeft(right);
+              setInputRight(left);
+            }}
+          >
+            &harr;
+          </Button>
+        </div>
+        <div>&nbsp;</div>
+      </div>
+
+      <div className="grid-inputs">
         <TextInput onUpdate={setInputLeft} value={inputLeft} />
         <TextInput onUpdate={setInputRight} value={inputRight} />
       </div>

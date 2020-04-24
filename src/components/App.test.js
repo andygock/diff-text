0c18832c800559665a4abda680e6a2347017bfb6
Andy Gock <andy@gock.net>
2020-04-25 00:10:43 +1000

    Update test cases

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders heading', () => {
   const { getByText } = render(<App />);
-  const element = getByText(/diff-text/i);
+  const heading = getByText(/^diff-text - A web app /i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('renders option buttons', () => {
+  const { getByText } = render(<App />);
+  const element = getByText('Chars'); // not the best way
   expect(element).toBeInTheDocument();
 });

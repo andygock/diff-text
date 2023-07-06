f8c5dc9cec55d1b4d09fcb9fbf45b85416778dd9
Andy Gock <andy@gock.net>
2023-07-07 07:10:03 +1000

    Fix heading margin, add working toast module

--- src/library/toaster.js
+++ src/library/toaster.js
@@ -1,20 +1,31 @@
-/* eslint-disable import/prefer-default-export */
+const toaster = document.createElement("div");
+toaster.style.position = "fixed";
+toaster.style.top = "0";
+toaster.style.left = "50%";
+toaster.style.transform = "translateX(-50%)";
+toaster.style.backgroundColor = "darkred";
+toaster.style.color = "#fff";
+toaster.style.padding = "10px";
+toaster.style.borderRadius = "5px";
+toaster.style.zIndex = "9999";
+toaster.style.transition = "opacity 0.5s ease-in-out";
+toaster.style.fontSize = "1.2rem";
 
-/* import { Position, Intent, OverlayToaster } from "@blueprintjs/core";
-
-const toaster = OverlayToaster.create({
-  position: Position.TOP,
-  className: "recipe-toaster",
-});
+const showToast = (message) => {
+  toaster.textContent = message;
+  document.body.appendChild(toaster);
+  setTimeout(() => {
+    toaster.style.opacity = "1";
+    setTimeout(() => {
+      toaster.style.opacity = "0";
+      setTimeout(() => {
+        document.body.removeChild(toaster);
+      }, 500);
+    }, 3000);
+  }, 100);
+};
 
 export const showError = (message) => {
-  console.error(message);
-  toaster.show({
-    message,
-    intent: Intent.DANGER,
-  });
-}; */
-
-export const showError = (message) => {
-  console.error("TODO: showError()", message);
+  // console.error("TODO: showError()", message);
+  showToast(message);
 };

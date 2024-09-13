c6167130fe08a0491ba0a1c63397f352c916db98
Andy Gock <andy@gock.net>
2024-09-13 17:02:56 +1000

    Deploy

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  // max file to reader 10 MB - large files may cause ReactDiffViewer to crash
-  maxFileSize: 1024 * 1024 * 10, // 10 MB
-
-  // when reading lots of lines, it often causes ReactDiffViewer to crash
-  maxLines: 10000,
-
-  maxPermittedLineLength: 10000,
-};
